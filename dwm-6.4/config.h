b1220fe724d9b06d3fe2bbb76c109b570fae85d2
Brandon Knape <d.brandonk@gmail.con>
2023-04-30 10:48:19 -0500

    Added commands to config.h. Made dwm.desktop better.

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -58,14 +58,19 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+
 static const char *firefox[]  = { "firefox", NULL };
 static const char *tmux[]  = { "st", "-e", "tmux", NULL };
+static const char *sgpt[]  = { "st", "-e", "sgpt --repl temp", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+
 	{ MODKEY,                       XK_t,      spawn,          {.v = tmux } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = firefox } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = sgpt } },
+
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
